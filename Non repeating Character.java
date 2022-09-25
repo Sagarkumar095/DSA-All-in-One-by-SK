fe4fa6941b8de47b97d98f66648831b970521916
Sagar Kumar <89767836+Sagarkumar095@users.noreply.github.com>
2022-09-25 16:06:01 +0530

    Create Non repeating Character.java

---
+++ Non repeating Character.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String st = sc.next();
+            
+            char ans=new Solution().nonrepeatingCharacter(st);
+            
+            if(ans!='$')
+            System.out.println(ans);
+            else
+            System.out.println(-1);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the first non-repeating character in a string.
+    static char nonrepeatingCharacter(String S)
+    {
+        //Your code here
+        int h[]=new int [26];
+        Arrays.fill(h,0);
+        // To store count of each character
+        for(int i=0;i<S.length();i++)
+        {
+            h[S.charAt(i)-'a']++;
+            
+        }
+        // check which char is non repeating
+        for(int i=0;i<S.length();i++)
+        {
+            if(h[S.charAt(i)-'a']==1)
+            {
+                return S.charAt(i);
+            }
+        }
+        return '$';
+    }
+}
+
